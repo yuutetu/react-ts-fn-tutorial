609e85e6d7f488cdb8584889a575fecf26db24e7
yuutetu <yuutetu@gmail.com>
2021-06-12 16:35:31 +0900

    Winner

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useState, useMemo } from "react";
 import Board from "./Board";
 
 type Props = {
@@ -9,12 +9,23 @@ const Game: React.VFC<Props> = (props) => {
     const [squares, setSquares] = useState(Array<string | null>(9).fill(null))
     const [xIsNext, setXIsNext] = useState(true)
     const handleClick = (i: number) => {
+        if (squares[i] != null || calculateWinner(squares)) {
+            return;
+        }
+
         const baseSquares = squares.slice()
         baseSquares[i] = xIsNext ? "X" : "O"
         setSquares(baseSquares)
         setXIsNext(!xIsNext)
     };
-    const status = 'Next player: ' + (xIsNext ? "X" : "O");
+    const status = (() => {
+        const winner = calculateWinner(squares)
+        if (winner) {
+            return "Winner: " + winner
+        } else {
+            return 'Next player: ' + (xIsNext ? "X" : "O")
+        }
+    })();
 
     return (
         <div className="game">
@@ -27,6 +38,26 @@ const Game: React.VFC<Props> = (props) => {
             </div>
         </div>
     )
+}
+
+const calculateWinner = (squares: Array<string | null>) => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 export default Game;
